c252830b9fde4c5b926dab0e313b18211b1938d2
mtueih <mtueih.org@outlook.com>
2025-07-01 18:53:33 +0800

    完成创建文件简单命令行工具 - touch，更新类函数宏头文件

---
+++ my-c-tools/mydefs.h
@@ -0,0 +1,10 @@
+#ifndef MYDEFS_H
+#define MYDEFS_H
+
+// 取两数最大值
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+// 取两数最小值
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+#endif  // MYDEFS_H
--- my-c-tools/mytouch/main.c
+++ my-c-tools/mytouch/main.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include "mydefs.h"
 
 int main(int argc, char ** argv) {
+    register unsigned char i;
+    FILE * p_file_0;
+    int ch;
 
     if (argc < 2) {
-        fprintf(stderr, "\n\tUsage: touch <file1> [file2] ...\t\n");
+        fprintf(stderr, "\n\t:Usage: touch <file1> [file2] ...\t\n");
         return EXIT_FAILURE;
     }
 
-    for (register unsigned i = 1; i < argc; ++i) {
-        
+    for (i = 1; i < MIN(argc, UCHAR_MAX); ++i) {
+        // 尝试打开文件——用于检查文件是否已存在
+        p_file_0 = fopen(argv[i], "r");
+        if (p_file_0 != NULL) {
+            fclose(p_file_0);
+
+            printf("文件“%s”已存在，是否覆盖？[y/N]", argv[i]);
+
+            ch = getchar();
+            if (ch != '\n') while (getchar() != '\n') continue;
+            if (ch != 'y' && ch != 'Y') {
+                continue;
+            }
+        }
+
+        p_file_0 = fopen(argv[i], "w");
+        if (p_file_0 == NULL) {
+            fprintf(stderr, "文件“%s”创建失败！\n", argv[i]);
+        } else {
+            fclose(p_file_0);
+            printf("文件“%s”创建成功！\n", argv[i]);
+        }
     }
 
 
